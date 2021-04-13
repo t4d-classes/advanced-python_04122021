64a4ef5ae99c6db5489eb6b1ecc78c6fb707471c
Eric Greene <eric@t4d.io>
2021-04-13 08:42:16 -0400

    add mypy

--- rates_client/rates_client/__main__.py
+++ rates_client/rates_client/__main__.py
@@ -1,13 +1,31 @@
 """Main Module"""
+from datetime import date, timedelta
+from typing import Generator
+import holidays
 
-from rates_client.some_pkg.some_mod import do_it
+
+def business_days(start_date: date,
+                  end_date: date) -> Generator[date, None, None]:
+    """ determines the business days for a given date range """
+
+    us_holidays = holidays.UnitedStates()
+
+    for num in range((end_date - start_date).days + 1):
+        the_date = start_date + timedelta(days=num)
+        if (the_date.weekday() < 5) and (the_date not in us_holidays):
+            yield the_date
 
 
 def main() -> None:
     """Main Function"""
 
-    do_it()
-    print("Welcome to Package rates_client")
+    start_date = date(2019, 1, 1)
+    end_date = date(2019, 2, 28)
+
+    working_days = business_days(start_date, end_date)
+
+    for working_day in working_days:
+        print(working_day.strftime("%A, %d %B %Y"))
 
 
 if __name__ == '__main__':
