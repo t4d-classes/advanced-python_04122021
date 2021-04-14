c85bddf0249e426c4cd59f103b7b204b63b241ec
Eric Greene <eric@t4d.io>
2021-04-14 15:40:20 -0400

    add exercise

--- rates_client_server/rate_server.py
+++ rates_client_server/rate_server.py
@@ -1,25 +1,65 @@
 """ rate server module """
 from typing import Optional
+from multiprocessing.sharedctypes import Synchronized  # type: ignore
 import multiprocessing as mp
 import socket
+import threading
 import sys
 
+# Add support the following client command
 
-# Create "ClientConnectionThread" class that inherits from "Thread"
+# GET 2019-01-03 EUR
 
-# Each time a client connects, a new thread should be created with the
-# "ClientConnectionThread" class. The class is responsible for sending the
-# welcome message and interacting with the client, echoing messages
+# GET is the command name
+# 2019-01-03 is the date of the current rates to retrieve
+# EUR is the currency symbol to retrieve, DO NOT USE USD
 
-# Use a multiprocessing shared "Value" object to track the count of
-# connected clients
-# increment the count when a client connects, and decrement the count when
-# a client disconnects
-# add new server command named "count" that displays the count of
-# connected clients
+# Call the Rates API using the USD as the base to get the currency rate
+# for the specified year
+
+# Ideally your code will do the following:
+
+# 1. Use a regular expression with named capture groups to extract parts
+# of the command
+
+# 2. Add a function named "process_client_command" to
+# "ClientConnectionThread" that will process the parsed command including
+# calling the API, extracting the API response, and send back the rate
+# value to the client
+
+# 3. Send back an error message for an incorrectly formatted command or an
+# unsupported command name (only the GET command is supported)
 
 
-def rate_server(host: str, port: int) -> None:
+class ClientConnectionThread(threading.Thread):
+    """ client connection thread """
+
+    def __init__(self,
+                 conn: socket.socket,
+                 client_count: Synchronized,
+                 ) -> None:
+        threading.Thread.__init__(self)
+        self.conn = conn
+        self.client_count = client_count
+
+    def run(self) -> None:
+
+        self.conn.sendall(b"Connected to the Rate Server")
+
+        try:
+            while True:
+                data = self.conn.recv(2048)
+                if not data:
+                    break
+                self.conn.sendall(data)
+        except OSError:
+            pass
+
+        with self.client_count.get_lock():
+            self.client_count.value -= 1
+
+
+def rate_server(host: str, port: int, client_count: Synchronized) -> None:
     """rate server"""
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_server:
@@ -28,18 +68,15 @@ def rate_server(host: str, port: int) -> None:
         socket_server.bind((host, port))
         socket_server.listen(100)
 
-        conn, _ = socket_server.accept()
+        while True:
 
-        conn.sendall(b"Connected to the Rate Server")
+            conn, _ = socket_server.accept()
 
-        try:
-            while True:
-                data = conn.recv(2048)
-                if not data:
-                    break
-                conn.sendall(data)
-        except OSError:
-            pass
+            client_con_thread = ClientConnectionThread(conn, client_count)
+            client_con_thread.start()
+
+            with client_count.get_lock():
+                client_count.value += 1
 
 
 class RateServerError(Exception):
@@ -76,11 +113,18 @@ def command_server_status(server_process: Optional[mp.Process]) -> None:
         print("server is stopped")
 
 
+def command_client_count(client_count: int) -> None:
+    """ command client count """
+
+    print(f"client count: {client_count}")
+
+
 def main() -> None:
     """Main Function"""
 
     try:
 
+        client_count: Synchronized = mp.Value('i', 0)
         server_process: Optional[mp.Process] = None
 
         while True:
@@ -89,13 +133,16 @@ def main() -> None:
 
             if command == "start":
                 server_process = mp.Process(target=rate_server,
-                                            args=("localhost", 5000))
+                                            args=("localhost", 5000,
+                                                  client_count))
                 command_start_server(server_process)
             elif command == "stop":
                 command_stop_server(server_process)
                 server_process = None
             elif command == "status":
                 command_server_status(server_process)
+            elif command == "count":
+                command_client_count(client_count.value)
             elif command == "exit":
                 if server_process and server_process.is_alive():
                     server_process.terminate()
