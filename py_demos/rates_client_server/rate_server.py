26677de2d1322c3d1401b0343cac7842d51dc78b
Eric Greene <eric@t4d.io>
2021-04-14 15:29:35 -0400

    add code demos

--- py_demos/rates_client_server/rate_server.py
+++ py_demos/rates_client_server/rate_server.py
@@ -1,11 +1,42 @@
 """ rate server module """
 from typing import Optional
+from multiprocessing.sharedctypes import Synchronized # type: ignore
 import multiprocessing as mp
 import sys
 import socket
+import threading
+
+class ClientConnectionThread(threading.Thread):
+    """ client connection thread """
+
+    def __init__(self, conn: socket.socket, client_count: Synchronized) -> None:
+        threading.Thread.__init__(self)
+        self.conn = conn
+        self.client_count = client_count
+
+    def run(self) -> None:
+        self.conn.sendall(b"Connected to the Rate Server")
+
+        try:
+            while True:
+                data = self.conn.recv(2048)
+                if not data:
+                    break
+                self.conn.sendall(data)
+        except OSError:
+            pass
+        
+        # with self.client_count.get_lock():
+        #     self.client_count.value -= 1
+
+        c_count = self.client_count.value
+
+        c_count -= 1
+
+        self.client_count.value = c_count
 
 
-def rate_server(host: str, port: int) -> None:
+def rate_server(host: str, port: int, client_count: Synchronized) -> None:
     """rate server"""
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_server:
@@ -14,29 +45,27 @@ def rate_server(host: str, port: int) -> None:
         socket_server.bind((host, port))
         socket_server.listen(10)
 
-        conn, _ = socket_server.accept()
-        conn.sendall(b"Connected to the Rate Server")
+        while True:
 
-        try:
-            while True:
-                data = conn.recv(2048)
-                if not data:
-                    break
-                conn.sendall(data)
-        except OSError:
-            pass
+            conn, _ = socket_server.accept()
+
+            client_con_thread = ClientConnectionThread(conn, client_count)
+            client_con_thread.start()
+
+            with client_count.get_lock():
+                client_count.value += 1
 
 class RateServerError(Exception):
     """ rate server error class """
 
 
-def command_start_server(server_process: Optional[mp.Process]) -> mp.Process:
+def command_start_server(server_process: Optional[mp.Process], client_count: Synchronized) -> mp.Process:
     """ command start server """
 
     if server_process and server_process.is_alive():
         print("server is already running")
     else:
-        server_process = mp.Process(target=rate_server, args=('127.0.0.1', 5000))
+        server_process = mp.Process(target=rate_server, args=('127.0.0.1', 5000, client_count))
         server_process.start()
         print("server started")
 
@@ -58,7 +87,12 @@ def command_server_status(server_process: Optional[mp.Process]) -> None:
     if server_process and server_process.is_alive():
         print("server is running")
     else:
-        print("server is stopped")        
+        print("server is stopped")
+
+def command_client_count(client_count: int) -> None:
+    """ command client count """
+
+    print(f"client count: {client_count}")   
 
 
 def main() -> None:
@@ -66,6 +100,7 @@ def main() -> None:
 
     try:
 
+        client_count: Synchronized = mp.Value('i', 0)
         server_process: Optional[mp.Process] = None
 
         while True:
@@ -73,10 +108,12 @@ def main() -> None:
             command = input("> ")
 
             if command == "start":
-                server_process = command_start_server(server_process)
+                server_process = command_start_server(server_process, client_count)
             elif command == "stop":
                 command_stop_server(server_process)
                 server_process = None
+            elif command == "count":
+                command_client_count(client_count.value)
             elif command == "status":
                 command_server_status(server_process)
             elif command == "exit":
